4ca55578e491b4aee09acd4d6ace769dd2f4bdf5
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-04-28 18:20:14 +0200

    exemple of usage of a task for updating the UI

---
+++ 1-31_Threads_UpdatingUI/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _1_31_Threads_UpdatingUI
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private double computeAverages(long noOfValues)
+        {
+            double total = 0;
+            Random rand = new Random();
+
+            for (long values = 0; values < noOfValues; values++)
+            {
+                total = total + rand.NextDouble();
+            }
+
+            return total / noOfValues;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            long noOfValues = long.Parse(NumberOfValuesTextBox.Text);
+            Task.Run(() =>
+            {
+                double result = computeAverages(noOfValues);
+
+                ResultTextBlock.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    ResultTextBlock.Text = "Result: " + result.ToString();
+                });
+            }
+            );
+        }
+    }
+}
